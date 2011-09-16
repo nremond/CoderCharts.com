a9f70884779e08f685b3b212a7b33d1d18c94732
nire <nicolas.remond@gmail.com>
2011-09-16 11:24:35 +0200

    use set() instead of a dic

--- EnenyOfMyEneny/enemy.py
+++ EnenyOfMyEneny/enemy.py
@@ -2,17 +2,17 @@ import sys
 with open(sys.argv[1],'r') as f: 
 	l = f.readline().split()
 	ee = {}
-	for _ in range(0, int(l[1])):
+	def up(a,b):
+		if a in ee: ee[a].append(b)
+		else: ee[a] = [b]
+	for _ in xrange(0, int(l[1])):
 		(x,y) = tuple(f.readline().split())
-		def up(a,b):
-			if a in ee: ee[a].append(b)
-			else: ee[a] = [b]
 		up(x,y)
 		up(y,x)
 	for n in [f.readline().strip() for _ in range(0, int(f.readline()))]: 
 		en = ee[n]
-		friends = {}
+		friends = set()
 		for e in en:
 			for f in ee[e]:
-				if f!=n and f not in en : friends[f] = 0		
-		print len(friends.keys())
+				if f!=n and f not in en : friends.add(f)		
+		print len(friends)
