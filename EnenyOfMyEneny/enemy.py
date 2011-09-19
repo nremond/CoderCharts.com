c03fdad0975cc3dc2006d2f2916b74546e656e86
nire <nicolas.remond@gmail.com>
2011-09-19 12:09:31 +0200

    371 points ... I still need to reduce the code

--- EnenyOfMyEneny/enemy.py
+++ EnenyOfMyEneny/enemy.py
@@ -1,18 +1,14 @@
+#!/usr/bin/python
 import sys
+from collections import defaultdict
 with open(sys.argv[1],'r') as f: 
-	l = f.readline().split()
-	ee = {}
-	def up(a,b):
-		if a in ee: ee[a].append(b)
-		else: ee[a] = [b]
-	for _ in xrange(0, int(l[1])):
-		(x,y) = tuple(f.readline().split())
-		up(x,y)
-		up(y,x)
-	for n in [f.readline().strip() for _ in range(0, int(f.readline()))]: 
-		en = ee[n]
-		friends = set()
-		for e in en:
-			for f in ee[e]:
-				if f!=n and f not in en : friends.add(f)		
-		print len(friends)
+	l=f.readline().split()
+	E=defaultdict(set)
+	for _ in range(0, int(l[1])):
+		(x,y)=tuple(f.readline().split())
+		E[y].add(x)
+		E[x].add(y)
+	f.readline()
+	for n in f.readlines():
+		n=n.strip()
+		print len(set([k for e in E[n] for k in E[e]-set([n])-E[n]]))
