a70794fc145568d59d2ca80d1b09e2b6a7e0bd26
nire <nicolas.remond@gmail.com>
2011-10-24 22:09:29 +0200

    this works ... but still need to reduce the size of the code

--- BarrelCypher/barrel_cypher.py
+++ BarrelCypher/barrel_cypher.py
@@ -1,23 +1,15 @@
-import sys,string
-a=string.ascii_lowercase
-t=dict(enumerate(a))
+import sys
+t=list("abcdefghijklmnopqrstuvwxyz")
 l=open(sys.argv[1]).readlines()
 for n in range(int(l[0])):
-	K=[int(j) for j in list(l[2*n+1].strip())]
+	K=[int(j) for j in list(l[2*n+1][:-1])]
 	K=K+K[::-1]
 	S=l[2*n+2].strip()
 	b=""
 	i=0
 	for s in S:
-		#(a.index(c)+1)%26
-		if s==' ':
-			b+=' '
+		if s==' ': b+=s
 		else:
-			print "(%s, %i, %i)" (s, a.index(s), K[i])
-		
-			t[(a.index(s)+K[i])%26]
-			b+= "(%s, %s)" % (s, t[(a.index(s)+K[i])%26])
-			i+=1
-			i=i%len(K)
+			b+=t[(t.index(s)-K[i]+26)%26]
+			i=(i+1)%len(K)
 	print b
-
